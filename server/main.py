c77067246f1f354882c4f0fd6f0fc3534cf206a0
ahnopologetic <stahn1995@gmail.com>
2025-05-02 23:47:00 -0400

    fix: add allow origin

--- server/main.py
+++ server/main.py
@@ -128,7 +128,7 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
 # Add middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://sfl.onrender.com", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
