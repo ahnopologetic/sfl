4c539e3ef365c1ef869cafd852871fc4b2a58983
ahnopologetic <stahn1995@gmail.com>
2025-05-02 23:48:48 -0400

    refactor: update allow origins

--- server/main.py
+++ server/main.py
@@ -128,7 +128,7 @@ class RateLimitMiddleware(BaseHTTPMiddleware):
 # Add middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://sfl.onrender.com", "http://localhost:3000"],
+    allow_origins=["https://sfl-blue.vercel.app", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
